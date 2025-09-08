8929f91ff53be624f03c66a9fdca4ab191065e22
ayeshaafifa2025 <ayeshaafifa2025@gmail.com>
2025-09-08 11:43:52 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+}
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./styles//*.{css}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         inter: ["Inter", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     ".//*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         inter: ["Inter", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
